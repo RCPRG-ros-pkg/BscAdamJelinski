1f03035157694ce41a5ab203bd96948819622f5b
Adam Jeliński <adas.jelinski@gmail.com>
2024-05-08 02:47:47 +0200

    build: initialize packages

---
+++ joystick_control/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = "joystick_control"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Adam Jeliński",
+    maintainer_email="adas.jelinski@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "joystick_multiplexer = joystick_control.joystick_multiplexer:main"
+        ],
+    },
+)
